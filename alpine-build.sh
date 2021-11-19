279ba6b6d6c59751b79b0f4a20fa3bae12582341
Jim Patterson <jpatterson@datatrac.com>
2021-11-19 18:04:23 -0500

    Add more version to prebuilt actions

--- alpine-build.sh
+++ alpine-build.sh
@@ -15,22 +15,24 @@ _HOMEDIR="${PWD}"
 docker build \
     --tag compiler \
     - <<EOT
-FROM mhart/alpine-node:${_NODE_VER}
+FROM node:${_NODE_VER}-alpine
 
 # Need compilers to build
 RUN apk add --no-cache \
         make gcc g++ python3 automake autoconf libtool
 
-# add matching user
-RUN addgroup -g ${_GID} user
-RUN adduser -u ${_UID} -G user -h "${_HOMEDIR}" -H -D user
+# add matching user if needed
+RUN grep -q -E ":${_GID}:" /etc/group || \
+    addgroup -g ${_GID} user
+RUN grep -q -E ":x:${UID}:" /etc/passwd || \
+    adduser -u ${_UID} -G user -h "${_HOMEDIR}" -H -D user
 
 WORKDIR ${_HOMEDIR}
 EOT
 
 docker run \
     --rm \
-    --user user \
+    --user ${_UID} \
     -v "$_HOMEDIR:$_HOMEDIR" \
     compiler:latest \
     ash -c "npm ci --build-from-source && ./node_modules/.bin/node-pre-gyp package"
